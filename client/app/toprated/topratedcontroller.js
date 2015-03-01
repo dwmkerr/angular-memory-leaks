2e396e1cdc9322e1b30db1dfcfaae883ac563f18
Dave Kerr <dwmkerr@gmail.com>
2015-03-01 18:11:46 +0100

    Wrote up the analysis of scenario 2

--- client/app/toprated/topratedcontroller.js
+++ client/app/toprated/topratedcontroller.js
@@ -1,6 +1,11 @@
 angular.module('app')
 .controller('TopRatedController', function($scope, $http, $interval) {
 
+  //  Create a class, assign it to the scope. This'll help us 
+  //  see if $scope is leaked.
+  function TopRatedControllerTag() {}
+  $scope.__tag = new TopRatedControllerTag();
+
   var refresh = function() {
 
     $http.get('/api/toprated').success(function(albums) {
